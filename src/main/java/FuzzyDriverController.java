8bf40e4f034e882f9c367dc9474edfc70d48929b
Szymon Świerk <swierk.szymon99@gmail.com>
2020-12-17 01:02:05 +0100

    done

--- src/main/java/FuzzyDriverController.java
+++ src/main/java/FuzzyDriverController.java
@@ -2,12 +2,16 @@ import net.sourceforge.jFuzzyLogic.FIS;
 
 public class FuzzyDriverController {
 
-    public FIS fis;
+    private final FIS fis;
+    private final boolean showGraphs = false;
 
     public FuzzyDriverController(String fuzzyDriverPath){
         fis = FIS.load(fuzzyDriverPath, false);
         if( fis == null ) {
             System.err.println("Can't load file: '" + fuzzyDriverPath + "'");
+        }
+        if(showGraphs) {
+            fis.getFuzzyRuleSet().chart();
         }
     }
 
@@ -16,6 +20,6 @@ public class FuzzyDriverController {
         fis.getFuzzyRuleSet().setVariable("distance", distanceFromVehicle);
         fis.getFuzzyRuleSet().setVariable("slipperiness", slipperiness);
         fis.getFuzzyRuleSet().evaluate();
-        return fis.getFuzzyRuleSet().getVariable("speed_change").defuzzify();
+        return fis.getFuzzyRuleSet().getVariable("speed_difference").defuzzify();
     }
 }
